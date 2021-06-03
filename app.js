784c50a749f607c901d472c0f4bbefec2ed46551
Lixian <weiyuanleeif@gmail.com>
2021-06-03 15:08:22 -0400

    javascript done but a little buggy

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-function AddBtn() {
-document.getElementById("addBtn")
-}
+const doList = document.querySelector(".do-list");
+const inputBox = document.querySelector(".inputbox");
+const addButton = document.querySelector(".addBtn");
+const removeButton = document.querySelector(".removeBtn");
+const doListChild = document.querySelector(".taskitem")
+
+addButton.addEventListener("click", function(){
+const newLi = document.createElement('li');
+const liContent = document.createTextNode(inputBox.value);
+newLi.appendChild(liContent);
+doList.appendChild(newLi);
+});
+
+removeButton.addEventListener("click", function() {
+const parentList = document.querySelector('.do-list');
+parentList.removeChild(parentList.lastElementChild);
+});
+
