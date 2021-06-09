f9fbf0ac6080f20c3f4f8ca89f0d9fa3b136f588
Lixian <weiyuanleeif@gmail.com>
2021-06-08 21:45:41 -0400

    fixed add button and cleaned up css

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const doList = document.querySelector(".do-list");
 const inputBox = document.querySelector(".inputbox");
-const addButton = document.querySelector(".addBtn");
+const addBtn = document.querySelector(".addButton");
 const removeButton = document.querySelector(".removeBtn");
 const doListChild = document.querySelector(".taskitem")
 
-addButton.addEventListener("click", function(){
+addBtn.addEventListener("click", function(){
 const newLi = document.createElement('li');
 const liContent = document.createTextNode(inputBox.value);
 newLi.appendChild(liContent);
