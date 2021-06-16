7e76e0285dabc1604cea7dcc7c85b0e80aab926a
Lixian <weiyuanleeif@gmail.com>
2021-06-15 22:00:38 -0400

    fixed my check button JS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const doListChild = document.querySelector('.task-item');
 const deleteBtn = document.querySelectorAll('.delete-task');
 const checkBtn = document.querySelector('.checked-task');
 const listDiv = document.querySelector(".to-do-list");
-
+const editBtn = document.querySelector('edit-task')
 form.addEventListener('submit', function(e) {
   //initialize list item and store task
   e.preventDefault()
@@ -18,17 +18,30 @@ form.addEventListener('submit', function(e) {
   //Add buttons to list-item
   let checkBtn = document.createElement('button')
   let deleteBtn = document.createElement('button')
+  let editBtn = document.createElement('button')
+
   checkBtn.classList.add('checked-task')
   deleteBtn.classList.add('delete-task')
+  editBtn.classList.add('edit-task')
+
   deleteBtn.addEventListener('click', deleteTask)
+  checkBtn.addEventListener('click', checkTask)
+  editBtn.addEventListener('click', editTask)
   //insert fontawesome icon
   checkBtn.innerHTML = `
     <i class="fas fa-check"></i>
   `
   //insert X text
-  deleteBtn.innerText = 'X'
+  deleteBtn.innerHTML = `
+  <i class="fas fa-trash-alt"></i>
+`
+editBtn.innerHTML = `
+<i class="fas fa-pen"></i>
+`
+
   taskItem.appendChild(checkBtn)
   taskItem.appendChild(deleteBtn)
+  taskItem.appendChild(editBtn)
   doList.appendChild(taskItem)
 })
 
@@ -37,25 +50,16 @@ function deleteTask() {
   let currentTarget = this.parentNode
   currentTarget.remove()
 }
-
 deleteBtn.forEach(btn => btn.addEventListener('click', deleteTask))
 
-/*
-I need to create a delete button for each item in the list
+//check btn
+function checkTask() {
+let currentTarget = this.parentNode
+currentTarget.classList.toggle("completed")
 
-First I need to grab my list of objects and put it in the nodelist for just a moment
-then develop a loop that goes through the nodelist length
-then i need to create a button
-then i need to attach the button to my list
-I need to do that for every item in the list
-*/
-/*
-let myNodeList = document.getElementsByTagName('ul'); 
-let i;
-for(i = 0; i < myNodeList.length; i++) {
-    let newDeleteBtn = document.createElement("button");
-    newDeleteBtn.innerHTML = "X";
-    newDeleteBtn.className = "delete_task";
-    myNodeList[i].appendChild(newDeleteBtn);
 }
-*/
+function editTask() {
+  let editedTask = document.querySelector("li")
+  editedTask.contentEditable = true
+}
+
